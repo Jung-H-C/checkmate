d42ccb481fe2af2b8262e1b620e71b61685b804c
xotourllife <xotourllife@naver.com>
2022-12-05 20:49:51 +0900

    tutorials 구현

--- app/src/main/java/com/example/checkmate/tutorials.java
+++ app/src/main/java/com/example/checkmate/tutorials.java
@@ -1,17 +1,32 @@
 package com.example.checkmate;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
+import android.widget.Button;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class tutorials extends AppCompatActivity {
 
+    Button to_main;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         supportRequestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.tutorials);
+
+        to_main = (Button) findViewById(R.id.tutorials_btn_to_main);
+
+        to_main.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(view.getContext(), MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
