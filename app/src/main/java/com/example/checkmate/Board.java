1c2258de542ad7b0498050fd97af96157d930fa3
xotourllife <xotourllife@naver.com>
2022-12-11 14:55:26 +0900

    소스코드 추출용

--- app/src/main/java/com/example/checkmate/Board.java
+++ app/src/main/java/com/example/checkmate/Board.java
@@ -1,11 +1,6 @@
 package com.example.checkmate;
 
 public class Board {
-    public Board (int x, int y, Unit unit) {
-        pos_x = x;
-        pos_y = y;
-        unitInside = unit;
-    }
 
     public Board (int x, int y) {
         pos_x = x;
@@ -15,26 +10,13 @@ public class Board {
     int pos_y;
     Unit unitInside;
 
-    int get_pos_x() {
-        return pos_x;
-    }
-
-    int get_pos_y() {
-        return pos_y;
-    }
-
-    void set_pos(int x, int y) {
-        pos_x = x;
-        pos_y = y;
-    }
-
     boolean IsOccupied() {
-        // 해당 칸에 유닛이 있는지를 확인
+        // 해당 칸에 유닛이 있는지를 판별
         if (unitInside == null) return false;
         else if(unitInside.isDead) return false;
         else return true;
     }
-    // 이미지 경로를 불러오는 get_img_src()함수
+    // 해당 칸의 이미지 경로를 불러오는 get_img_src()함수
     String get_img_src() {
         if (unitInside != null) return unitInside.img_src;
         else return "drawable/board_button";
--- app/src/main/java/com/example/checkmate/Player.java
+++ app/src/main/java/com/example/checkmate/Player.java
@@ -9,13 +9,14 @@ public class Player implements Serializable {
     String profile_img;
 
     Unit[] units;
+
     // units 스태틱 변수로 놓으면 안됨! 해당 player만 사용!
     public Player() {
         this.profile_name = "default";
         this.profile_img = "default";
 
         this.units = new Unit[5];
-        for (int i = 0; i<5; i++) {
+        for (int i = 0; i < 5; i++) {
             this.units[i] = new Unit();
         }
         // Horse, Queen, king, Ghost, Car 순
@@ -31,44 +32,4 @@ public class Player implements Serializable {
         this.units[3].setImg_src("drawble/ghost_a");
         this.units[4].setImg_src("drawble/car_a");
     }
-
-    public Player(String name, String profile_img) {
-        this.profile_name = name;
-        this.profile_img = profile_img;
-
-        this.units = new Unit[5];
-        for (int i = 0; i<5; i++) {
-            this.units[i] = new Unit();
-        }
-        // Horse, Queen, king, Ghost, Car 순
-        this.units[0].setName("Horse");
-        this.units[1].setName("Queen");
-        this.units[2].setName("King");
-        this.units[3].setName("Ghost");
-        this.units[4].setName("Car");
-
-        this.units[0].setImg_src("drawble/horse_a");
-        this.units[1].setImg_src("drawble/queen_a");
-        this.units[2].setImg_src("drawble/king");
-        this.units[3].setImg_src("drawble/ghost_a");
-        this.units[4].setImg_src("drawble/car_a");
-    }
-
-    public void setUnitStatus(String name, int no, boolean flag) {
-        // no는 해당 말의 초기 위치
-        if (flag) { // A팀 일때,
-            units[no] = new Unit(no, 0, name, true);
-        }
-        else {
-            units[no] = new Unit(no, 6, name, false);
-        }
-    }
-//    public Unit getUnit(int pos_x, int pos_y) {
-//        Unit SpecifiedUnit;
-//        for (int i = 0; i < 5; i++) {
-//            if(pos_x == this.units[i].getPos_x() && pos_y == this.units[i].getPos_y()) {
-//                SpecifiedUnit = this.units[i];
-//            }
-//        }
-//    } 구현할 필요는 없을거 같음.
 }
